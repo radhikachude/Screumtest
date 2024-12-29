838a0601f41de9a8b65211b4f085de9971e3d85b
radhikachude <76487409+radhikachude@users.noreply.github.com>
2024-12-29 22:35:36 +0530

    Update TestProjectApplication.java

--- src/main/java/com/springsecurity/TestProject/TestProjectApplication.java
+++ src/main/java/com/springsecurity/TestProject/TestProjectApplication.java
@@ -2,15 +2,15 @@ package com.springsecurity.TestProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient; // Ensure this import is added
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableDiscoveryClient		
+@EnableDiscoveryClient // Enables service registration and discovery
 @ComponentScan(basePackages = {"com.springsecurity.TestProject"})
 public class TestProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestProjectApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TestProjectApplication.class, args);
+    }
 }
